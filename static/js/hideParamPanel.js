8840151c0d317f2530e4a7df7fe811ca33ca70a0
Lera <Lera.53rus@yandex.ru>
2025-05-02 00:05:46 +0300

    hide param panel btn

---
+++ static/js/hideParamPanel.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const hideBtn = document.querySelector('.hide-param-btn');
+    const parameters = document.querySelector('.parameters');
+    const canvas_block = document.querySelector('.canvas');
+
+    hideBtn.addEventListener('click', () => {
+        const isHidden = parameters.style.display === 'none';
+        parameters.style.display = isHidden ? 'block' : 'none';
+
+        const screenWidth = window.innerWidth;
+
+        if (screenWidth < 1050) {
+            canvas_block.style.width = '100%';
+        } else {
+            canvas_block.style.width = isHidden ? '80%' : '100%';
+        }
+    });
+});
