0fa73f849361def3380df3b8744876e7879e571e
Lera <Lera.53rus@yandex.ru>
2025-06-28 14:03:29 +0300

    changed db

--- config.py
+++ config.py
@@ -9,9 +9,17 @@ import os
 
 
 # база данных в яндекс облаке
+# class Config:
+#     SQLALCHEMY_DATABASE_URI = (
+#         "postgresql://superuser:Zerg6161@rc1a-o7dtdmmh5abl2bik.mdb.yandexcloud.net:6432/edicationDB"
+#     )
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+#     SECRET_KEY = 'K5s2G9jP4FvA7rL3tQ1W6eD8'
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = (
-        "postgresql://superuser:Zerg6161@rc1a-o7dtdmmh5abl2bik.mdb.yandexcloud.net:6432/edicationDB"
-    )
+    # Путь к файлу базы данных SQLite
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'K5s2G9jP4FvA7rL3tQ1W6eD8'
